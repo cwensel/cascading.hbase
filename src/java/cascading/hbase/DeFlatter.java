861a67c4f5bf39e41706a5b0750b5dcdc4422977
Chris K Wensel <chris@wensel.net>
2009-03-13 18:05:10 -0700

    remove @override on interface methods

--- src/java/cascading/hbase/DeFlatter.java
+++ src/java/cascading/hbase/DeFlatter.java
@@ -1,6 +1,5 @@
 package cascading.hbase;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 import cascading.flow.FlowProcess;
@@ -8,94 +7,91 @@ import cascading.operation.BaseOperation;
 import cascading.operation.Function;
 import cascading.operation.FunctionCall;
 import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
 import cascading.tuple.TupleEntry;
 import cascading.tuple.TupleEntryCollector;
-import cascading.tuple.Tuple;
-import cascading.tuple.Tuples;
-import cascading.util.Util;
-
+import org.apache.hadoop.hbase.io.BatchUpdate;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.apache.hadoop.hbase.io.BatchUpdate;
-//import org.apache.hadoop.hbase.io.Cell;
-import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 //import org.apache.hadoop.hbase.io.RowResult;
 
 /**
- * 
- */
-
-/**
- * @author erik
  *
  */
-public class DeFlatter extends BaseOperation implements Function {
+
+/** @author erik */
+public class DeFlatter extends BaseOperation implements Function
+  {
   /** Field LOG */
-  private static final Logger LOG = LoggerFactory.getLogger(DeFlatter.class);
-  
+  private static final Logger LOG = LoggerFactory.getLogger( DeFlatter.class );
+
   /** Field groupFieldSelector */
 //  private Fields groupFieldSelector;
   /** Field resultFieldSelectors */
 //  private Fields[] resultFieldSelectors;
 //  private String family = "";
-  
-  
-  /**
-   * Constructor that takes no arguments
-   *
-   */  
-  public DeFlatter() {
-    super(Fields.ARGS);
-  }
 
-  
+
+  /** Constructor that takes no arguments */
+  public DeFlatter()
+    {
+    super( Fields.ARGS );
+    }
+
+
   /**
    * Constructor
-   * @param fieldDeclaration, the fields to process
    *
-   */  
-  public DeFlatter(Fields fieldDeclaration) {
-    super(fieldDeclaration);
-  }
- 
+   * @param fieldDeclaration, the fields to process
+   */
+  public DeFlatter( Fields fieldDeclaration )
+    {
+    super( fieldDeclaration );
+    }
+
 
   /* (non-Javadoc)
    * @see cascading.operation.Function#operate(cascading.flow.FlowProcess, cascading.operation.FunctionCall)
    */
-  @Override
-  public void operate(FlowProcess flow, FunctionCall fun) {
-    System.out.println("\tDeFlatter: operate ");
+  public void operate( FlowProcess flow, FunctionCall fun )
+    {
+    System.out.println( "\tDeFlatter: operate " );
     TupleEntry input = fun.getArguments();
     TupleEntryCollector outputCollector = fun.getOutputCollector();
 
-    Tuple tup = new Tuple(input.getTuple());
+    Tuple tup = new Tuple( input.getTuple() );
     Iterator<ImmutableBytesWritable> iter = input.getTuple().iterator();
-    
+
     //Getting rowName
-    BatchUpdate bu =  null;
-    if(iter.hasNext()){
-      bu = new BatchUpdate(iter.next().get());
-    }
+    BatchUpdate bu = null;
+    if( iter.hasNext() )
+      {
+      bu = new BatchUpdate( iter.next().get() );
+      }
 
     //Putting keys and value in BatchUpdate
     byte[] key = null;
     byte[] val = null;
-    while(iter.hasNext()){
+    while( iter.hasNext() )
+      {
       key = iter.next().get();
-      if(iter.hasNext()){
+      if( iter.hasNext() )
+        {
         val = iter.next().get();
-      } else {
-        val = null;
-        if(LOG.isInfoEnabled()){
-          LOG.info(
-            "Tuple to BatchUpdate ended with a key, setting value to null");
         }
+      else
+        {
+        val = null;
+        if( LOG.isInfoEnabled() )
+          {
+          LOG.info( "Tuple to BatchUpdate ended with a key, setting value to null" );
+          }
+        }
+      System.out.println( "key " + new String( key ) + ", val " + new String( val ) );
+      bu.put( key, val );
       }
-      System.out.println("key "+new String(key)+ ", val " +new String(val) );
-      bu.put(key, val);
+    outputCollector.add( new Tuple( bu ) );
     }
-    outputCollector.add(new Tuple(bu));
-  }
 
-}
+  }
